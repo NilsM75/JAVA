13fe0ae82451e5dc6d14b52db5a8fa051c831e95
samielatri <sami.elatri@yahoo.com>
2019-12-02 15:37:21 +0100

    partie 1 débuggé

--- Jeu.java
+++ Jeu.java
@@ -1,19 +1,15 @@
 public class Jeu {
 	public static void main (String [] args){
-		
-		/* Test complet */
-		
+		/* Test demandé */
 		Pomme p1 = new Pomme() ;
-		System.out.println(p1) ; 
 		Sac sac1 = new Sac() ;
-		System.out.println(sac1) ; 
 		Sac sac2 = new Sac(5) ;
-		System.out.println(sac2) ; 
-		sac2.ajouter(p1) ; 
-		sac2.ajouter(sac1) ; 
-		System.out.println(sac2) ; 
-		Sac sac3 = sac2.obtenir(1) ; 
-		System.out.println(sac3) ; 
+		
+		sac2.ajouter(p1) ;
+		sac2.ajouter(sac1) ;
+		sac1.ajouter(p1) ; 
+		System.out.println(sac2) ;
+		/* Tests en plus */
 		
 		return ; 
 	}
