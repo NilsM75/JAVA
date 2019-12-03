c4b94c360907c86215fff2634fce3f1f8372e0d2
samielatri <sami.elatri@yahoo.com>
2019-12-03 10:48:50 +0100

    correction Noms qui ne marchait pas

--- Noms.java
+++ Noms.java
@@ -1,32 +1,39 @@
-public  class Noms { // classe outil 
+public abstract class Noms { // classe outil 
 	/* Attributs */
-	private String tabNoms[] ; // tableau qui contiendra les noms choisis
+	private String[] tabNoms ; // tableau qui contiendra les noms choisis
 	private static int cpt = 0 ; // compteur du nombre de fois que le tableau est parcouru
-	private static int position = (0) ; // donne la position courante dans le tableau
+	private static int position = (-1) ; // donne la position courante dans le tableau
 
 	/* Constructeurs */
 	
 	public Noms() {
-		String tabNoms[] = {"Jean", "Petit", "QuiDanse"} ;
+		tabNoms = new String[] {"Jean", "Petit", "QuiDanse"} ;
 	}
 	
 	/* Methodes */
 	
-  public String getNom(){ // marche pas
-    if(cpt > 0){
-      return (""+tabNoms[position++]+cpt);
-    }
-    if(position == tabNoms.length){
-      cpt++;
-      return (""+tabNoms[position++]);
-    }
-    return (""+tabNoms[position++]);
-  }
-
+	public String getNom(){ 
+		position ++ ; 
+		if (position < tabNoms.length){
+			if(cpt == 0){
+				return ("" + tabNoms[position]) ; 
+			}
+			return ("" + tabNoms[position] + cpt) ; 
+		}
+		
+		cpt ++ ; 
+		position = (-1) ; 
+		
+		return getNom() ; 
+	}
+	
+	/* //Pour tester la fonction getNom(), il suffit de décommenter cette partie et de rendre la classe non abstraite 
 	public static void main(String[] args) {
 		Noms test = new Noms() ;
-		System.out.print(test.getNom() + "  " + test.getNom() + "  " + test.getNom()) ;  
-		
+		System.out.println(test.getNom() + "  " + test.getNom() + "   " + test.getNom()+
+		test.getNom() + "  " + test.getNom() + "   " + test.getNom()
+		+test.getNom() + "  " + test.getNom() + "   " + test.getNom()+
+		test.getNom() + "  " + test.getNom() + "   " + test.getNom()) ; 
 	}
-
+	*/
 }
