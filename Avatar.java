397909462863fd669c1715be628159d71e7f14f1
samielatri <sami.elatri@yahoo.com>
2019-12-04 14:45:12 +0100

    partie update

---
+++ Avatar.java
@@ -0,0 +1,134 @@
+import java.util.ArrayList;
+
+public class Avatar extends Personnage { // Avatar est un personnage 
+	/* Attributs */
+	private ArrayList<Creature> listeAmis ; // liste d'amis
+	private ArrayList<Acc> listeAcc ; // liste d'accessoires
+
+	/* Constructeurs */
+	public Avatar (String nom, double poids){
+		super(nom, poids) ; // Appel au constructeur Personnage(nom, poids) 
+		listeAmis = new ArrayList<Creature>() ;
+		listeAcc = new ArrayList<Acc>() ;
+	}
+
+	/* Méthodes */
+	@Override
+	public String toString(){
+		// String s = String.format("%.2f", poids); (?)
+		
+		String temp1, temp2 ; 
+		
+		if(listeAmis.size() > 1){
+			temp1 = new String (" amis ") ;
+		}else{
+			temp1 = new String (" ami ") ; 
+		}
+		
+		if(listeAcc.size() > 1){
+			temp2 = new String (" accessoires") ; 
+		}else{
+			temp2 = new String (" accessoire") ; 
+		}
+		
+		return (super.toString() + " " + listeAmis.size() + temp1 + listeAcc.size() + temp2) ;
+	}
+	
+	public boolean estAmi (Creature c){
+		for(int i = 0 ; i < listeAmis.size() ; i ++){
+			if(listeAmis.get(i) == c){
+				return true ;
+			}
+		}
+		return false ;
+	}
+	
+	public void devenirAmi (Creature c){
+		if (estAmi(c)){
+			return ; 
+		}
+		
+		listeAmis.add(c) ;
+		System.out.println(this.toString() + "devient ami avec " + c.toString()) ; 
+		
+		return ; 
+	}
+	
+	public void perdreAmi (Creature c){
+		if (!estAmi(c)){
+			return ; 
+		}
+		
+		listeAmis.remove(c) ; 
+		System.out.println(this.toString() + "a perdu " + c.toString()) ; 
+		
+		return ; 
+	}
+	
+	public void rencontrer (Creature c){
+		/*  Lorsque l'avatar rencontre une créature, il lui offre le premier accessoire
+			qu'il a dans la liste d'accessoires et la créature ajoute cet accessoire dans son
+			sac. 
+			Si la créature n'est pas amie avec lui et si l'accessoire offert a un poids supérieur
+			à 0.5 kg alors la créature devient amie avec lui. Si, par contre, il n'a pas d'accessoires à offrir
+			à la créature alors si cette créature était une amie, il perd son amitié. */
+		
+		Acc a ; // accessoire temporaire
+		int i = 0 ; // compteur
+		// On cherche le premier accessoire dans la liste 
+		while (a == null){
+			if (i == listeAcc.size()){ // pas d'acccessoire à offrir
+				if (estAmi(c)){ 
+					perdreAmi(c) ;
+				}
+				return ; 
+			}
+			
+			a = listeAcc.get(i) ; 
+			i ++ ; 
+		}
+		// Si  on est là c'est qu'on a le premier accesseoire a et ne sont pas amis 
+		if (a.getPoids() > 0.5) {
+			devenirAmi(c) ;
+			c.ajouterAcc(a) ; 
+		}
+		return ; 
+	}
+	
+	
+	public double course (){ // fait manger puis courir pendant 1 heure chaque créature l'une après l'autre et retourne la distance totale parcourue
+		Creature c ;
+		double distance = 0.0 ; //distance en km
+		for(int i = 0 ; i < listeAmis.size() ; i ++){
+			c = listeAmis.get(i) ;
+			c.manger() ; // mange 
+			c.courir() ; // court pendant 1 heure
+			distance += c.getVitesse() ; 
+		}
+		
+		return distance ;
+	}
+	
+	public Creature getCreaturePlusRapide(){ // créature qui court le plus vite 
+		Creature tmpRapide = listeAmis.get(0) ;
+		
+		for(int i = 1 ; i < listeAmis.size() ; i ++){
+			if(tmpRapide.getVitesse() < listeAmis.get(i).getVitesse()) {
+				tmpRapide = listeAmis.get(i);
+			}
+		}
+		
+		return tmpRapide;
+	}
+	
+	public int compterAccMangeable(){ // retourne le nombre d'accessoires mangeables 
+		int cpt = 0 ;
+		for (Acc a : listeAcc){
+			if (a instanceof Mangeable){
+				cpt ++ ;
+			}
+		}
+		return cpt ;
+	}
+
+}
--- Creature.java
+++ Creature.java
@@ -1,15 +1,52 @@
-public class Creature extends Personnage { // Creature est un personnage qui a un sac 
-	private Sac leSac ; // sac du personnage pouvant contenir entre 1 et 10 accessoires
-	private String nom ; // nom du personnage 
-	
-	public Creature(){
-		leSac = new Sac(10) ;
-		nom = getNom() ; 
-	}
-	
-	
-	public double getVitesse(){
-		return (1.0 / 4.0) * vitesse - getPoids ;
-	}
-	
+public class Creature extends Personnage{
+  /*Attributs*/
+  private Sac sacCreature ;
+
+  /*Constructeurs*/
+  public Creature (){
+    super(Noms.getNom());
+    sacCreature = new Sac(); //Le sac peut de la creature peut contenir entre 1 et 10 accessoires
+    //this.nom = getNom();
+  }
+
+  /*Methodes*/
+  public void ajouterAcc (Acc a){
+    if((sacCreature.getPoids()+a.getPoids())<(0.5*super.poids)) {
+      sacCreature.ajouter(a);
+    } else { System.out.println("Trop lourd!"); }
+  }
+
+  public double getVitesse(){
+    double v = (0.25*super.poids)-(sacCreature.getPoids());
+    if (v < 0){
+      return 0;
+    }
+    return v;
+  }
+
+  public void manger (){
+    Acc tab [] = this.sacCreature.getSac();
+    for(int i=0; i<tab.length; i++){
+      if(tab[i] instanceof Mangeable){
+        Acc temp = tab[i];
+        manger((Mangeable) temp);
+        tab[i] = null;
+        System.out.println(super.nom+" a mangé "+temp.toString());
+      }
+    }
+  }
+
+  public void manger (Mangeable m){
+    super.poids += m.getPoids();
+  }
+
+  public String courir (){
+    String s = String.format("%.2f", this.getVitesse());
+    return (super.toString()+" court à la vitesse "+s+"km/h avec le "+sacCreature);
+  }
+  /* affiche par exemple :
+Bob1 51,0 kg court à vitesse 11,29 km/h avec sac No 4 1,46kg contient 2
+accessoires sur 10 places
+pomme No 5 0,38kg 4,5cm
+pomme No 6 1,08kg 6,4cm */
 }
