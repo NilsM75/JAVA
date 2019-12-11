00f91b44874b0470734eb7c8e9d18f1cbf9b3fc0
Nils Millot <compte.mxs@hotmail.fr>
2019-12-11 23:51:40 +0100

    la dalle de ouf

---
+++ BigMac.java
@@ -0,0 +1,26 @@
+public class BigMac extends Acc implements Mangeable {
+
+  /* Attributs */
+  private String[] tab = new String[] {"Bien gras", "Delicieux", "Fat de ouf"} ;
+  private double poids ; // poids de la pomme en kg
+  private String adjectif ;
+
+
+  /* Constructeurs */
+  public BigMac() {
+    super("BigMac") ; // Appel au cosntructeur Acc(categorie)
+    adjectif = tab [((int)Math.random()*3+0)] ;
+    poids = Math.random() ;
+  }
+
+  /* Méthodes */
+  @Override
+  public String toString(){
+    return String.format("%s", super.toString(), adjectif) ; // toString d'Acc
+    // exemple : pomme No 1 0.26kg 3.9cm
+  }
+
+  public double getPoids(){ // retourne le poids de la pomme
+    return poids ;
+  }
+}
