198ae91efd84266b5c19f7dc81d88a0219aa860d
Nils Millot <compte.mxs@hotmail.fr>
2019-12-11 22:44:41 +0100

    cc

--- Avatar.java
+++ Avatar.java
@@ -1,79 +1,79 @@
-import java.util.Scanner ; 
+import java.util.Scanner ;
 import java.util.ArrayList ;
 
 public class Avatar extends Personnage { // Avatar est un personnage qui a une liste d'amis et une liste d'accessoires
-	
+
 	/* Attributs */
-	
+
 	private ArrayList<Creature> listeAmis ; // liste d'amis (qui sont des créatues)
-	private ArrayList<Acc> listeAcc ; // liste d'accessoires 
-	private Monde monde ; // le monde dans lequel "évolue" l'Avatar 
-	
+	private ArrayList<Acc> listeAcc ; // liste d'accessoires
+	private Monde monde ; // le monde dans lequel "évolue" l'Avatar
+
 	/* Constructeurs */
-	
+
 	public Avatar (){
 		super(Noms.getNom()) ; // Appel au constructeur Personnage(nom)
-		listeAmis = new ArrayList<Creature>() ; 
-		listeAcc = new ArrayList<Acc>() ; 
-		monde = new Monde() ; 
-	}
-	
-	public Avatar (String nom, double poids){
-		super(nom, poids) ; // Appel au constructeur Personnage(nom, poids) 
 		listeAmis = new ArrayList<Creature>() ;
 		listeAcc = new ArrayList<Acc>() ;
-		monde = new Monde() ; 
+		monde = new Monde() ;
 	}
-	
+
+	public Avatar (String nom, double poids){
+		super(nom, poids) ; // Appel au constructeur Personnage(nom, poids)
+		listeAmis = new ArrayList<Creature>() ;
+		listeAcc = new ArrayList<Acc>() ;
+		monde = new Monde() ;
+	}
+
 	public Avatar (String nom){
 		super(nom) ; // Appel au constructeur Personnage(nom)
-		listeAmis = new ArrayList<Creature>() ; 
-		listeAcc = new ArrayList<Acc>() ; 
-		monde = new Monde() ; 
+		listeAmis = new ArrayList<Creature>() ;
+		listeAcc = new ArrayList<Acc>() ;
+		monde = new Monde() ;
 	}
 
 	public Avatar(Monde monde) {
 		this() ;
-		this.monde = monde ; 
+		this.monde = monde ;
 	}
-	
+
 	public Avatar (String nom, double poids, Monde monde){
-		this(nom, poids) ; 
-		this.monde = monde ; 
+		this(nom, poids) ;
+		this.monde = monde ;
 	}
-	
+
 	public Avatar(String nom, Monde monde){
-		this(nom) ; 
-		this.monde = monde ; 
+		this(nom) ;
+		this.monde = monde ;
 	}
-	
+
 	/* Méthodes */
-	
+
 	@Override
 	public String toString(){ // retourne chaine de caractères donnant des informations sur l'avatar
-		
-		// ami (s) / accessoire (s) 
-		
-		String temp1, temp2 ; 
-		
+
+		// ami (s) / accessoire (s)
+
+		String temp1, temp2 ;
+
 		if(listeAmis.size() > 1){
 			temp1 = new String (" amis ") ;
 		}else{
-			temp1 = new String (" ami ") ; 
+			temp1 = new String (" ami ") ;
 		}
-		
+
 		if(listeAcc.size() > 1){
-			temp2 = new String (" accessoires") ; 
+			temp2 = new String (" accessoires") ;
 		}else{
-			temp2 = new String (" accessoire") ; 
+			temp2 = new String (" accessoire") ;
 		}
-		
+
 		return (super.toString() + " " + listeAmis.size() + temp1 + listeAcc.size() + temp2) ;
-		/* Exemple : 
-			Jake 79.5 kg 1 ami 3 accessoires 
+		/* Exemple :
+			Jake 79.5 kg 1 ami 3 accessoires
 		*/
 	}
-	
+
 	public boolean estAmi (Creature c){ // retourne True si c est une amie, False sinon
 		for(int i = 0 ; i < listeAmis.size() ; i ++){
 			if((listeAmis.get(i)).equals(c)){
@@ -82,92 +82,92 @@ public class Avatar extends Personnage { // Avatar est un personnage qui a une l
 		}
 		return false ;
 	}
-	
+
 	public void devenirAmi (Creature c){ // fait devenir ami l'avatar avec la créature c (s'ils ne sont pas amis)
 		if (estAmi(c)){
-			return ; 
+			return ;
 		}
-		
+
 		listeAmis.add(c) ;
-		System.out.println(this.toString() + "devient ami avec " + c.toString()) ; 
-		
-		return ; 
+		System.out.println(this.toString() + "devient ami avec " + c.toString()) ;
+
+		return ;
 	}
-	
+
 	public void perdreAmi (Creature c){ // fait perdre l'avatar son ami c (s'ils sont amis)
 		if (!estAmi(c)){
-			return ; 
+			return ;
 		}
-		
-		listeAmis.remove(listeAmis.indexOf(c)) ; 
-		System.out.println(this.toString() + "a rompu son lien d'amitié avec " + c.toString()) ; 
-		
-		return ; 
+
+		listeAmis.remove(listeAmis.indexOf(c)) ;
+		System.out.println(this.toString() + "a rompu son lien d'amitié avec " + c.toString()) ;
+
+		return ;
 	}
-	
+
 	public void rencontrer (Creature c){
 		/*  Lorsque l'avatar rencontre une créature, il lui offre le premier accessoire
 			qu'il a dans la liste d'accessoires et la créature ajoute cet accessoire dans son
-			sac. 
+			sac.
 			Si la créature n'est pas amie avec lui et si l'accessoire offert a un poids supérieur
 			à 0.5 kg alors la créature devient amie avec lui. Si, par contre, il n'a pas d'accessoires à offrir
 			à la créature alors si cette créature était une amie, il perd son amitié. */
-		
+
 		int i = 0 ; // compteur
-		Acc a ; 
-		// On cherche le premier accessoire dans la liste 
+		Acc a ;
+		// On cherche le premier accessoire dans la liste
 		do{
 			if (i == listeAcc.size()){ // pas d'acccessoire à offrir
-				if (estAmi(c)){ 
+				if (estAmi(c)){
 					perdreAmi(c) ;
 				}
-				return ; 
+				return ;
 			}
-			
-			a = listeAcc.get(i) ; 
-			i ++ ; 
-		}while (a == null) ; 
-		
-		// Si  on est là c'est qu'on a le premier accesseoire a et ils ne sont pas amis 
+
+			a = listeAcc.get(i) ;
+			i ++ ;
+		}while (a == null) ;
+
+		// Si  on est là c'est qu'on a le premier accesseoire a et ils ne sont pas amis
 		if (a.getPoids() > 0.5) {
 			devenirAmi(c) ;
-			c.ajouterAcc(a) ; 
-			listeAcc.remove(listeAcc.indexOf(a)) ; 
+			c.ajouterAcc(a) ;
+			listeAcc.remove(listeAcc.indexOf(a)) ;
 		}
-		
-		return ; 
+
+		return ;
 	}
-	
-	
+
+
 	public double course (){ // fait manger puis courir pendant 1 heure chaque créature l'une après l'autre et retourne la distance totale parcourue
 		Creature c ;
 		double distance = 0.0 ; //distance en km
 		for(int i = 0 ; i < listeAmis.size() ; i ++){
 			c = listeAmis.get(i) ;
-			c.manger() ; // mange 
-			// court pendant 1 heure 
-			System.out.println("Pendant 1 heure : ") ; 
-			c.courir() ; 
-			distance += c.getVitesse() ; 
+			c.manger() ; // mange
+			// court pendant 1 heure
+			System.out.println("Pendant 1 heure : ") ;
+			c.courir() ;
+			distance += c.getVitesse() ;
 		}
-		
+
 		return distance ;
 	}
-	
-	public Creature getCreaturePlusRapide(){ // retourne la créature qui court le plus vite 
+
+	public Creature getCreaturePlusRapide(){ // retourne la créature qui court le plus vite
 		Creature tempRapide = listeAmis.get(0) ;
-		
+
 		for(int i = 1 ; i < listeAmis.size() ; i ++){
 			if(tempRapide.getVitesse() < (listeAmis.get(i)).getVitesse()) {
-				tempRapide = listeAmis.get(i); 
+				tempRapide = listeAmis.get(i);
 			}
 		}
-		
+
 		return tempRapide;
 	}
-	
-	public int compterAccMangeable(){ // retourne le nombre d'accessoires mangeables 
-		int cpt = 0 ; // compteur 
+
+	public int compterAccMangeable(){ // retourne le nombre d'accessoires mangeables
+		int cpt = 0 ; // compteur
 		for (int i = 0 ; i < listeAcc.size() ; i ++){
 			if (listeAcc.get(i) instanceof Mangeable){
 				cpt ++ ;
@@ -175,115 +175,115 @@ public class Avatar extends Personnage { // Avatar est un personnage qui a une l
 		}
 		return cpt ;
 	}
-	
-	public void ramasser(Acc a){ 
+
+	public void ramasser(Acc a){
 	/*  L'Avatar ramasse l'acessoire a :
 		L'accessoire a est supprmié du monde, a est mis dans la liste d'accessoires de l'avatar
 		Un message du style "X ramasse A" est affiché
 	*/
-		listeAcc.add(a) ; 
+		listeAcc.add(a) ;
 		monde.supprimerItem(a) ;
-		System.out.println(this.toString() + " ramasse " + a.toString()) ; 
-		return ; 
+		System.out.println(this.toString() + " ramasse " + a.toString()) ;
+		return ;
 	}
-	
+
 	public void rencontrerVoisins(){
 	/*	Pour chaque voisin de l'avatar
 		si le voisin est un accessoire, alors l'avatar ramasse l'accessoire;
 		si le voisin est une créature, alors l'avatar rencontre la créature;
-		si le voisin est un avatar, alors l'avatar salue (affiche une salutation) l'autre avatar 
+		si le voisin est un avatar, alors l'avatar salue (affiche une salutation) l'autre avatar
 		Attention : l'avatar ne doit pas se saluer lui même
 	*/
-		ArrayList<Item> tempVoisins = monde.getVoisins((Item) this) ; 
+		ArrayList<Item> tempVoisins = monde.getVoisins((Item) this) ;
 		Item tempVoisin ;
 		for (int i = 0 ; i < tempVoisins.size() ; i++){
-			tempVoisin = tempVoisins.get(i) ; 
+			tempVoisin = tempVoisins.get(i) ;
 			if (tempVoisin instanceof Acc){
-				ramasser((Acc) tempVoisin) ; 
+				ramasser((Acc) tempVoisin) ;
 			}
-			
+
 			if (tempVoisin instanceof Creature){
-				rencontrer((Creature) tempVoisin) ; 
+				rencontrer((Creature) tempVoisin) ;
 			}
-			
+
 			if (tempVoisin instanceof Avatar) {
 				if (tempVoisin == this){
 					System.out.println(this.toString() + " salue " + tempVoisin.toString()) ;
 				}
 			}
 		}
-		
-		return ; 
+
+		return ;
 	}
-	
+
 	public void seDeplacer(){
 	/*	Le programme demande à l'utilisateur de saisir dans le terminal une abscisse.
 		Si l'abscisse n'est pas dans le monde, alors le programme demande à nouveau de saisir une abscisse
 		jusqu'à ce que le nombre saisi soit correcte.
 		Si l'abscisse est dans le monde, alors on fait de même avec l'ordonnée.
-		L'utilisateur saisit toujours un nombre entier. 	
+		L'utilisateur saisit toujours un nombre entier.
 	*/
 
-		System.out.println("### Déplacement de " + this.getNomPersonnage() + " ###") ; 
-	
-		int valeurMax = (monde.getTaille() - 1) ; 
+		System.out.println("### Déplacement de " + this.getNomPersonnage() + " ###") ;
+
+		int valeurMax = (monde.getTaille() - 1) ;
 		int x, y ;
-		Scanner scanner = new Scanner(System.in) ; 
-		
+		Scanner scanner = new Scanner(System.in) ;
+
 		do{
-			System.out.println("Entrer une abscisse entre [0, " + valeurMax + "] : ") ; 
+			System.out.println("Entrer une ordonée entre [0, " + valeurMax + "] : ") ;
 			x = scanner.nextInt() ;
 			if ((x < 0) || (x > valeurMax)){
-				System.out.println("Saisie non valide ! ") ; 
+				System.out.println("Saisie non valide ! ") ;
 			}
 		} while ((x < 0) || (x > valeurMax)) ;
-		
+
 		do{
-			System.out.println("Entrer une ordonnée entre [0, " + valeurMax + "] : ") ; 
-			y = scanner.nextInt() ; 
+			System.out.println("Entrer une abscisse entre [0, " + valeurMax + "] : ") ; 
+			y = scanner.nextInt() ;
 			if ((y < 0) || (y > valeurMax)){
-				System.out.println("Saisie non valide ! ") ; 
+				System.out.println("Saisie non valide ! ") ;
 			}
-		} while ((y <0) || (y > valeurMax)) ; 
-		
-		System.out.print("Déplacement de " + this.getNomPersonnage() + " de (" + this.getX() + ", " + this.getY() + ") vers (") ; 
-		this.setX(x) ; 
+		} while ((y <0) || (y > valeurMax)) ;
+
+		System.out.print("Déplacement de " + this.getNomPersonnage() + " de (" + this.getX() + ", " + this.getY() + ") vers (") ;
+		this.setX(x) ;
 		this.setY(y) ;
-		System.out.println(this.getX() + ", " + this.getY() + ")") ; 
-		
-		// Quand il se déplace, il "rencontre" ses voisins 
+		System.out.println(this.getX() + ", " + this.getY() + ")") ;
+
+		// Quand il se déplace, il "rencontre" ses voisins
 		rencontrerVoisins() ;
-		return ; 
+		return ;
 	}
-	
-	/* Exemple : 
+
+	/* Exemple :
 		### Déplacement de Jake ###
-		Entrer une abscisse entre [0, 4] : 
+		Entrer une abscisse entre [0, 4] :
 		20
-		Entrer une abscisse entre [0, 4] : 
+		Entrer une abscisse entre [0, 4] :
 		3
-		Entrer une ordonnée entre [0, 4] : 
+		Entrer une ordonnée entre [0, 4] :
 		2
 		Déplacement de Jake de (1, 1) vers (3, 2)
 	*/
-	
+
 	/*
-	// Décommenter pour tester la méthode seDeplacer() 
+	// Décommenter pour tester la méthode seDeplacer()
 	public static void main(String[] args){
 		System.out.println("Test de la méthode seDeplacer()") ;
-		Avatar avatarTest = new Avatar() ; 
-		System.out.println("Voici notre avatar de test :\n" + avatarTest.toString()) ; 
-		System.out.println("seDeplacer()") ; 
-		avatarTest.seDeplacer() ; 
-		System.out.println("Fin du test de la méthode seDeplacer()") ; 
+		Avatar avatarTest = new Avatar() ;
+		System.out.println("Voici notre avatar de test :\n" + avatarTest.toString()) ;
+		System.out.println("seDeplacer()") ;
+		avatarTest.seDeplacer() ;
+		System.out.println("Fin du test de la méthode seDeplacer()") ;
 	}
 	*/
-	
+
 	public ArrayList<Creature> getListeAmis(){ // retourne liste amis
 		return listeAmis ;
 	}
-	
-	public ArrayList<Acc> getListeAcc(){ // tetourne liste d'accessoires 
+
+	public ArrayList<Acc> getListeAcc(){ // tetourne liste d'accessoires
 		return listeAcc ;
 	}
 }
--- Jeu.java
+++ Jeu.java
@@ -1,190 +1,190 @@
 public class Jeu {
 	public static void main (String [] args){ // Notre main de Jeu
-		// Partie 4 - Le jeu et ses améliorations 
-		
+		// Partie 4 - Le jeu et ses améliorations
+
 		// Q 15
-		
+
 		System.out.println("\nDébut du main du Jeu") ;
-		
-		// Création du monde 
-		Monde monde = new Monde() ;
-		System.out.println("Voici notre monde :\n" + monde.toString()) ; 
-		
-		// Populons ce monde 
-		// Ajout de 6 créatures dans le monde 
-		Creature creature1 = new Creature() ; 
-		monde.ajouterItem(creature1) ; 
-		//System.out.println("Créature1 ajoutée :\n" + creature1.toString()) ; 
-		Creature creature2 = new Creature() ; 
-		monde.ajouterItem(creature2) ; 
-		//System.out.println("Créature2 ajoutée :\n" + creature2.toString()) ; 
+
+		// Création du monde
+		Monde monde = new Monde(12) ;
+		System.out.println("Voici notre monde :\n" + monde.toString()) ;
+
+		// Populons ce monde
+		// Ajout de 6 créatures dans le monde
+		Creature creature1 = new Creature() ;
+		monde.ajouterItem(creature1) ;
+		//System.out.println("Créature1 ajoutée :\n" + creature1.toString()) ;
+		Creature creature2 = new Creature() ;
+		monde.ajouterItem(creature2) ;
+		//System.out.println("Créature2 ajoutée :\n" + creature2.toString()) ;
 		Creature creature3 = new Creature() ;
-		monde.ajouterItem(creature3) ; 		
-		//System.out.println("Créature3 ajoutée :\n" + creature3.toString()) ; 
-		Creature creature4 = new Creature() ; 
-		monde.ajouterItem(creature4) ; 
-		//System.out.println("Créature4 ajoutée :\n" + creature4.toString()) ; 
-		Creature creature5 = new Creature() ; 
-		monde.ajouterItem(creature5) ; 
-		//System.out.println("Créature5 ajoutée :\n" + creature5.toString()) ; 
-		Creature creature6 = new Creature() ; 
-		monde.ajouterItem(creature6) ; 
-		//System.out.println("Créature6 ajoutée :\n" + creature6.toString()) ; 		
-		// Ajout de 3 pommes dans le monde 
-		Pomme p1 = new Pomme() ; 
+		monde.ajouterItem(creature3) ;
+		//System.out.println("Créature3 ajoutée :\n" + creature3.toString()) ;
+		Creature creature4 = new Creature() ;
+		monde.ajouterItem(creature4) ;
+		//System.out.println("Créature4 ajoutée :\n" + creature4.toString()) ;
+		Creature creature5 = new Creature() ;
+		monde.ajouterItem(creature5) ;
+		//System.out.println("Créature5 ajoutée :\n" + creature5.toString()) ;
+		Creature creature6 = new Creature() ;
+		monde.ajouterItem(creature6) ;
+		//System.out.println("Créature6 ajoutée :\n" + creature6.toString()) ;
+		// Ajout de 3 pommes dans le monde
+		Pomme p1 = new Pomme() ;
 		monde.ajouterItem(p1) ;
-		//System.out.println("Pomme1 ajoutée :\n" + p1.toString()) ; 
-		Pomme p2 = new Pomme() ; 
-		monde.ajouterItem(p2) ; 
-		//System.out.println("Pomme2 ajoutée :\n" + p2.toString()) ; 
-		Pomme p3 = new Pomme() ; 
+		//System.out.println("Pomme1 ajoutée :\n" + p1.toString()) ;
+		Pomme p2 = new Pomme() ;
+		monde.ajouterItem(p2) ;
+		//System.out.println("Pomme2 ajoutée :\n" + p2.toString()) ;
+		Pomme p3 = new Pomme() ;
 		monde.ajouterItem(p3) ;
-		//System.out.println("Pomme3 ajoutée :\n" + p3.toString()) ; 
-		// Ajout de 3 sacs dans le monde 
-		Sac sac1 = new Sac() ; 
+		//System.out.println("Pomme3 ajoutée :\n" + p3.toString()) ;
+		// Ajout de 3 sacs dans le monde
+		Sac sac1 = new Sac() ;
 		monde.ajouterItem(sac1) ;
-		//System.out.println("sac1 ajouté :\n" + sac1.toString()) ; 
-		Sac sac2 = new Sac() ; 
-		monde.ajouterItem(sac2) ; 
-		//System.out.println("sac2 ajouté :\n" + sac2.toString()) ; 
-		Sac sac3 = new Sac() ; 
+		//System.out.println("sac1 ajouté :\n" + sac1.toString()) ;
+		Sac sac2 = new Sac() ;
+		monde.ajouterItem(sac2) ;
+		//System.out.println("sac2 ajouté :\n" + sac2.toString()) ;
+		Sac sac3 = new Sac() ;
 		monde.ajouterItem(sac3) ;
-		//System.out.println("sac3 ajouté :\n" + sac3.toString()) ; 
-		// Monde généré avec succès, affichage du monde 
-		System.out.println("Voici le monde peuplé : " + monde.toString()) ; 
-		System.out.println("Affichons ce monde : ") ; 
-		monde.afficher() ; 
-		
-		// Il est temps d'ajouter deux avatars représentant chacun un joueur humain 
-		Avatar joueur1 = new Avatar(monde) ; 
-		monde.ajouterItem(joueur1) ; 
-		//System.out.println("Ajout du premier avatar/joueur :\n" + joueur1.toString()) ; 
-		Avatar joueur2 = new Avatar(monde) ; 
-		monde.ajouterItem(joueur2) ; 
-		//System.out.println("Ajout du deuxième avatar/joueur :\n" + joueur2.toString()) ; 
-		
-		// réaffichage du monde 
-		//System.out.println("Voici le monde peuplé : " + monde.toString()) ; 
-		//System.out.println("Affichons ce monde : ") ; 
-		//monde.afficher() ; 
-		
+		//System.out.println("sac3 ajouté :\n" + sac3.toString()) ;
+		// Monde généré avec succès, affichage du monde
+		System.out.println("Voici le monde peuplé : " + monde.toString()) ;
+		System.out.println("Affichons ce monde : ") ;
+		monde.afficher() ;
+
+		// Il est temps d'ajouter deux avatars représentant chacun un joueur humain
+		Avatar joueur1 = new Avatar(monde) ;
+		monde.ajouterItem(joueur1) ;
+		//System.out.println("Ajout du premier avatar/joueur :\n" + joueur1.toString()) ;
+		Avatar joueur2 = new Avatar(monde) ;
+		monde.ajouterItem(joueur2) ;
+		//System.out.println("Ajout du deuxième avatar/joueur :\n" + joueur2.toString()) ;
+
+		// réaffichage du monde
+		//System.out.println("Voici le monde peuplé : " + monde.toString()) ;
+		//System.out.println("Affichons ce monde : ") ;
+		//monde.afficher() ;
+
 		// réalisation de 5 tours de jeu
 		System.out.println("monde : ") ; monde.afficher() ;
-		System.out.println("joueur1 : " + joueur1) ; 
+		System.out.println("joueur1 : " + joueur1) ;
 		System.out.println("joueur2 : " + joueur2) ;
 		System.out.println("\n\n\n") ;
 		for(int tour = 1 ; tour < 6 ; tour ++){
-			// Annonce du round 
+			// Annonce du round
 			System.out.println("*** Tour " + tour + " ***") ;
-			// tour du joueur 1 
-			System.out.println("Action de " + joueur1.getNomPersonnage()) ; 
-			joueur1.seDeplacer() ; 
-			System.out.println(joueur1) ; 
-			monde.afficher() ; 
-			// tour  du joueur 2 
-			System.out.println("Action de " + joueur2.getNomPersonnage()) ; 
+			// tour du joueur 1
+			System.out.println("Action de " + joueur1.getNomPersonnage()) ;
+			joueur1.seDeplacer() ;
+			System.out.println(joueur1) ;
+			monde.afficher() ;
+			// tour  du joueur 2
+			System.out.println("Action de " + joueur2.getNomPersonnage()) ;
 			joueur2.seDeplacer() ;
-			System.out.println(joueur2) ; 
-			monde.afficher() ; 
+			System.out.println(joueur2) ;
+			monde.afficher() ;
 		}
-		System.out.println("Fin") ; 
-		// réaffichage du monde 
-		//System.out.println("Voici le monde peuplé : " + monde.toString()) ; 
-		//System.out.println("Affichons ce monde : ") ; 
-		//monde.afficher() ; 
+		System.out.println("Fin") ;
+		// réaffichage du monde
+		//System.out.println("Voici le monde peuplé : " + monde.toString()) ;
+		//System.out.println("Affichons ce monde : ") ;
+		//monde.afficher() ;
 
-		
-		// réaffichage des joueurs  
+
+		// réaffichage des joueurs
 		//System.out.println("premier avatar/joueur :\n" + joueur1.toString()) ;
 		//System.out.println("deuxième avatar/joueur :\n" + joueur1.toString()) ;
-		
-		// Fin 5 tours, donc, il faut que chaque avatar réalise une course avec ses créatures 
-		System.out.println("\n\n\nScore Final") ; 
-		double j1 = joueur1.course() ; System.out.println("j1 = " + j1) ; 
-		double j2 = joueur2.course() ;  System.out.println("j2 = " + j2) ; 
+
+		// Fin 5 tours, donc, il faut que chaque avatar réalise une course avec ses créatures
+		System.out.println("\n\n\nScore Final") ;
+		double j1 = joueur1.course() ; System.out.println("j1 = " + j1 + "kilometres") ;
+		double j2 = joueur2.course() ;  System.out.println("j2 = " + j2 + "kilometres") ;
 		if (j1 == j2) {
 			System.out.println("Match nul") ;
 		}
 		if (j1 > j2) {
-			System.out.println("Joueur1 gagne !") ; 
+			System.out.println("Joueur1 gagne !") ;
 		}
 		if (j1 < j2) {
-			System.out.println("Joueur2 gagne !") ; 
+			System.out.println("Joueur2 gagne !") ;
 		}
-		
-		System.out.println("\nFin du main du jeu") ; 		
+
+		System.out.println("\nFin du main du jeu") ;
 		/* // décommenter pour voir les tests des demandés avant la Partie 4
-		System.out.println("Début du main de Jeu") ; 
-		// Test demandé 1 
-		System.out.println("\nTest demandé numéro 1") ; 
+		System.out.println("Début du main de Jeu") ;
+		// Test demandé 1
+		System.out.println("\nTest demandé numéro 1") ;
 		Pomme p1 = new Pomme() ;
-		System.out.println("Pomme p1 crée :\t" + p1) ; 
+		System.out.println("Pomme p1 crée :\t" + p1) ;
 		Sac sac1 = new Sac() ;
-		System.out.println("Sac sac1 crée :\t" + sac1) ; 
+		System.out.println("Sac sac1 crée :\t" + sac1) ;
 		Sac sac2 = new Sac(5) ;
 		System.out.println("Sac sac2 crée :\t" + sac2) ;
-		Pomme p2 = new Pomme() ; 
-		System.out.println("Pomme p2 crée :\t" + p2) ; 
-		System.out.println("Ajout de p1 dans sac2") ; 
+		Pomme p2 = new Pomme() ;
+		System.out.println("Pomme p2 crée :\t" + p2) ;
+		System.out.println("Ajout de p1 dans sac2") ;
 		sac2.ajouter(p1) ;
-		System.out.println("Ajout de sac1 dans sac2") ; 
+		System.out.println("Ajout de sac1 dans sac2") ;
 		sac2.ajouter(sac1) ;
-		System.out.println("Ajout de p2 dans sac1") ; 
-		sac1.ajouter(p2) ; 
+		System.out.println("Ajout de p2 dans sac1") ;
+		sac1.ajouter(p2) ;
 		System.out.println("Voici notre sac2 :\n" + sac2) ;
-		
+
 		// Test deamndé 2
 		System.out.println("\nTest demandé numéro 2") ;
-		
+
 		Creature c1 = new Creature() ;  // création d'une créature c1
 		System.out.println("La créature est crée :\n" + c1 + "\n") ; // affichage de la créature c1
-		// La créature c1 court 
-		System.out.println("La créature court \n") ; 
-		c1.courir() ;
-		System.out.println() ; 
-		
-		// On crée 3 pommes p11, p22 et p33 pour les ajouter dans le sac principal de la créature
-		Pomme p11 = new Pomme() ; 
-		Pomme p22 = new Pomme() ;
-		Pomme p33 = new Pomme() ; 
-		c1.ajouterAcc(p11) ; 
-		c1.ajouterAcc(p22) ; 
-		c1.ajouterAcc(p33) ; 
-		
-		// Et la créature court, de nouveau, mais avec 3 pommes dans son sac :
-		System.out.println("La créature court encore une fois mais avec 3 pommes dans son sac \n") ; 
+		// La créature c1 court
+		System.out.println("La créature court \n") ;
 		c1.courir() ;
 		System.out.println() ;
-		
-		// Maintenant, la créature va manger toutes les pommes dans son sac 
+
+		// On crée 3 pommes p11, p22 et p33 pour les ajouter dans le sac principal de la créature
+		Pomme p11 = new Pomme() ;
+		Pomme p22 = new Pomme() ;
+		Pomme p33 = new Pomme() ;
+		c1.ajouterAcc(p11) ;
+		c1.ajouterAcc(p22) ;
+		c1.ajouterAcc(p33) ;
+
+		// Et la créature court, de nouveau, mais avec 3 pommes dans son sac :
+		System.out.println("La créature court encore une fois mais avec 3 pommes dans son sac \n") ;
+		c1.courir() ;
+		System.out.println() ;
+
+		// Maintenant, la créature va manger toutes les pommes dans son sac
 		c1.manger() ;
 
-		// Maintenant, la créature va courir encore une fois après avoir mangé ses pommes 
-		System.out.println("La créature court après avoir manger toutes ses pommes \n") ; 
-		c1.courir() ; 
-		
+		// Maintenant, la créature va courir encore une fois après avoir mangé ses pommes
+		System.out.println("La créature court après avoir manger toutes ses pommes \n") ;
+		c1.courir() ;
+
 		// Test demandé 3
-		System.out.println("\nTest demandé numéro 3") ; 
-		Monde m = new Monde(5) ; // création d'un Monde de taille 5 
-		Creature cm1 = new Creature() ; 
-		System.out.println(cm1) ; 
-		Creature cm2 = new Creature() ; 
-		System.out.println(cm2) ; 
-		Creature cm3 = new Creature() ; 
-		System.out.println(cm3) ; 
+		System.out.println("\nTest demandé numéro 3") ;
+		Monde m = new Monde(5) ; // création d'un Monde de taille 5
+		Creature cm1 = new Creature() ;
+		System.out.println(cm1) ;
+		Creature cm2 = new Creature() ;
+		System.out.println(cm2) ;
+		Creature cm3 = new Creature() ;
+		System.out.println(cm3) ;
 		m.ajouterItem(cm1) ;
-		m.ajouterItem(cm2) ; 
-		m.ajouterItem(cm3) ; 
-		m.ajouterItem(new Pomme()) ; 
-		m.ajouterItem(new Pomme()) ; 
-		m.ajouterItem(new Pomme()) ; 
-		m.ajouterItem(new Avatar()) ; 
-		m.ajouterItem(new Avatar()) ; 
+		m.ajouterItem(cm2) ;
+		m.ajouterItem(cm3) ;
+		m.ajouterItem(new Pomme()) ;
+		m.ajouterItem(new Pomme()) ;
+		m.ajouterItem(new Pomme()) ;
 		m.ajouterItem(new Avatar()) ;
-		m.afficher() ; 
-		
-		System.out.println("\nFin du main de Jeu") ; 
+		m.ajouterItem(new Avatar()) ;
+		m.ajouterItem(new Avatar()) ;
+		m.afficher() ;
+
+		System.out.println("\nFin du main de Jeu") ;
 		*/
-		return ; 
+		return ;
 	}
 }
