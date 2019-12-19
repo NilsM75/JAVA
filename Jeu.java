14aea5bb489148d8edecf3f928ce001802e3b8a0
ELATRI <3801805@ppti-14-502-11.ufr-info-p6.jussieu.fr>
2019-12-19 10:13:16 +0100

    debug

--- Jeu.java
+++ Jeu.java
@@ -20,19 +20,141 @@ public class Jeu {
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		//Création du monde en mode panneau 
-		Monde m = new Monde(NB_CASES, TAILLE_CASE);
+		Monde monde = new Monde(NB_CASES, TAILLE_CASE);
 
-		f.setContentPane(m); // le monde est ajouté à la fenêtre
+		f.setContentPane(monde); // le monde est ajouté à la fenêtre
 		f.pack(); // la dite fenêtre est adaptée au panneau 
 		f.setVisible(true); // visible OK 
 		
 		System.out.println("@\t|Créatures et Avatars|\t@");
 
-		m.repaint(); 
+		monde.repaint(); 
 
 		/* Tours de jeu. */
 		//m.repaint() à chaque tour
+// Création du monde
 
+		//System.out.println("Voici notre monde :\n" + monde.toString()) ;
+
+		// Populons ce monde
+		// Ajout de 6 créatures dans le monde **************************************
+		Creature creature1 = new Creature() ;
+		monde.ajouterItem(creature1) ;
+		//System.out.println("Créature1 ajoutée :\n" + creature1.toString()) ;
+		Creature creature2 = new Creature() ;
+		monde.ajouterItem(creature2) ;
+		//System.out.println("Créature2 ajoutée :\n" + creature2.toString()) ;
+		Creature creature3 = new Creature() ;
+		monde.ajouterItem(creature3) ;
+		//System.out.println("Créature3 ajoutée :\n" + creature3.toString()) ;
+		Creature creature4 = new Creature() ;
+		monde.ajouterItem(creature4) ;
+		//System.out.println("Créature4 ajoutée :\n" + creature4.toString()) ;
+		Creature creature5 = new Creature() ;
+		monde.ajouterItem(creature5) ;
+		//System.out.println("Créature5 ajoutée :\n" + creature5.toString()) ;
+		Creature creature6 = new Creature() ;
+		monde.ajouterItem(creature6) ;
+		//System.out.println("Créature6 ajoutée :\n" + creature6.toString()) ;
+		RonaldMcDonald ronald1 = new RonaldMcDonald();
+		monde.ajouterItem(ronald1) ;
+
+		// Ajout de 3 pommes dans le monde **************************************
+		Pomme p1 = new Pomme() ;
+		monde.ajouterItem(p1) ;
+		//System.out.println("Pomme1 ajoutée :\n" + p1.toString()) ;
+		Pomme p2 = new Pomme() ;
+		monde.ajouterItem(p2) ;
+		//System.out.println("Pomme2 ajoutée :\n" + p2.toString()) ;
+		Pomme p3 = new Pomme() ;
+		monde.ajouterItem(p3) ;
+		//System.out.println("Pomme3 ajoutée :\n" + p3.toString()) ;
+
+		// Ajout de BigMac dans le Monde **************************************
+
+
+		// Ajout de 3 sacs dans le monde **************************************
+		Sac sac1 = new Sac() ;
+		monde.ajouterItem(sac1) ;
+		//System.out.println("sac1 ajouté :\n" + sac1.toString()) ;
+		Sac sac2 = new Sac() ;
+		monde.ajouterItem(sac2) ;
+		//System.out.println("sac2 ajouté :\n" + sac2.toString()) ;
+		Sac sac3 = new Sac() ;
+		monde.ajouterItem(sac3) ;
+		//System.out.println("sac3 ajouté :\n" + sac3.toString()) ;
+
+		// Monde généré avec succès, affichage du monde
+		//System.out.println("Voici le monde peuplé : " + monde.toString()) ;
+		//System.out.println("Affichons ce monde : ") ;
+		//monde.afficher() ;
+
+
+		// Il est temps d'ajouter deux avatars représentant chacun un joueur humain
+		Avatar joueur1 = new Avatar(monde) ;
+		monde.ajouterItem(joueur1) ;
+		//System.out.println("Ajout du premier avatar/joueur :\n" + joueur1.toString()) ;
+		Avatar joueur2 = new Avatar(monde) ;
+		monde.ajouterItem(joueur2) ;
+		//System.out.println("Ajout du deuxième avatar/joueur :\n" + joueur2.toString()) ;
+
+		// réaffichage du monde
+		//System.out.println("Voici le monde peuplé : " + monde.toString()) ;
+		//System.out.println("Affichons ce monde : ") ;
+		//monde.afficher() ;
+
+
+System.out.println("Monde : ") ; monde.afficher() ;
+		System.out.println("joueur1 : " + joueur1) ;
+		System.out.println("joueur2 : " + joueur2) ;
+		System.out.println("\n\n\n") ;
+		for(int tour = 1 ; tour < 3 ; tour ++){
+			// Annonce du round
+			System.out.println("*** Tour " + tour + " ***") ;
+			// tour du joueur 1
+			System.out.println("Action de " + joueur1.getNomPersonnage()) ;
+			joueur1.seDeplacer() ;
+			System.out.println("Maj j1 :" + joueur1) ;
+			monde.afficher() ;
+			// tour  du joueur 2
+			System.out.println("Action de " + joueur2.getNomPersonnage()) ;
+			joueur2.seDeplacer() ;
+			System.out.println("Maj j2 :" + joueur2) ;
+			monde.afficher() ;
+			try{
+			Thread.sleep(1000) ; // Ralenti l'affichage
+			} catch (InterruptedException e){
+				System.out.println(e.getMessage()) ; 
+			}
+			monde.repaint() ; // Redessine le graphique
+		}
+
+		System.out.println("Fin") ;
+		// réaffichage du monde
+		//System.out.println("Voici le monde peuplé : " + monde.toString()) ;
+		//System.out.println("Affichons ce monde : ") ;
+		//monde.afficher() ;
+
+
+		// réaffichage des joueurs
+		//System.out.println("premier avatar/joueur :\n" + joueur1.toString()) ;
+		//System.out.println("deuxième avatar/joueur :\n" + joueur1.toString()) ;
+
+		// Fin 5 tours, donc, il faut que chaque avatar réalise une course avec ses créatures
+		System.out.println("\n\n\n*************************** Score Final ***************************\n") ;
+		double j1 = joueur1.course() ; System.out.println("j1 = " + j1 + " kilometres") ;
+		double j2 = joueur2.course() ;  System.out.println("j2 = " + j2 + " kilometres") ;
+		if (j1 == j2) {
+			System.out.println("Match nul") ;
+		}
+		if (j1 > j2) {
+			System.out.println("Joueur1 gagne !") ;
+		}
+		if (j1 < j2) {
+			System.out.println("Joueur2 gagne !") ;
+		}
+
+		System.out.println("\nFin du main du jeu") ;
 
 		System.exit(0);
 		
